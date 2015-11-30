9a1f2c9305862f4c38a8ca2897640c827c89ba6b
Karl Gibson <kgibson@unidesk.com>
2015-11-30 18:08:09 -0500

    setup file share script

---
+++ SetupFileShare.ps1
@@ -0,0 +1,15 @@
+
+$dataDisks = Get-PhysicalDisk | Where CanPool
+$storSub = Get-StorageSubSystem
+$totalSize = ($dataDisks | Select -ExpandProperty Size | Measure-Object -Sum).Sum
+
+$storagePool = New-StoragePool -FriendlyName UnideskStoragePool -PhysicalDisks $dataDisks -StorageSubSystemFriendlyName $storSub.FriendlyName
+New-VirtualDisk -StoragePoolFriendlyName $storagePool.FriendlyName -ResiliencySettingName Simple -Size $totalSize -ProvisioningType Thin -FriendlyName UnideskVirtualDisk
+
+$newDisk = Get-Disk | Where FriendlyName -eq "Microsoft Storage Space Device"
+$newDisk | Initialize-Disk -PartitionStyle MBR
+$newPartition = $newDisk | New-Partition -AssignDriveLetter -UseMaximumSize
+$newVolume = $newPartition | Format-Volume -FileSystem NTFS -NewFileSystemLabel "UnideskFileShare" -Confirm:$false
+
+$sharePath = $newVolume.DriveLetter + ":\"
+New-SmbShare -Name Share -Path $sharePath -FullAccess $env:username
